21e06d2f81973d587d362c7a1540b32c8bc0a2fb
GigantPro2000 <92632623+GigantPro@users.noreply.github.com>
2022-11-23 14:38:25 +0300

    init

--- config.py
+++
@@ -1,7 +0,0 @@
-chunk_size      = 50
-big_chunk_size  = 16
-zoom            = 1
-speed           = 50
-fps             = 60
-debug           = True
-seed            = None
--- main.py
+++
@@ -1,5 +0,0 @@
-from game_core import Core
-
-
-if __name__ == '__main__':
-    Core().start_game()
