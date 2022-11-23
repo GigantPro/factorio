6a89b4e8c167b25096bb9bb34ab1759a7cc97380
Alexander Svetlov <92632623+GigantPro@users.noreply.github.com>
2022-11-23 14:25:58 +0300

    @#"$%^6&?**

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-cell_size      = 50
-chunk_size     = 16
+cell_size       = 50
+chunk_size      = 16
 zoom            = 1
-min_zoom = 0.4
+min_zoom        = 0.4
 speed           = 50
 fps             = 60
 debug           = True
