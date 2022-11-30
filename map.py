d4faa1844cc5427bffd6ae332ebbb24f5c4b042e
prMaRKoFpr <69719971+prMaRKoFpr@users.noreply.github.com>
2022-11-30 12:53:14 +0300

    Worked map version

--- map.py
+++ map.py
@@ -7,7 +7,7 @@ from perlin_noise import PerlinNoise
 
 
 class Map:
-    # в ините можно загрузить мапу
+    #в ините можно загрузить мапу
     def __init__(self, seed: int = random.randint(10000, 1000000)) -> None:
         self.seed = seed
         self.camera = None
@@ -31,7 +31,8 @@ class Map:
         return self.map
 
     def create_new_map(self):
-        self.map['0, 0'] = self._create_chunk(*self.player.get_coords())
+        self.generate_visible_chunks((0, 0))
+
 
     def set_camera(self, camera):
         self.camera = camera
@@ -45,16 +46,28 @@ class Map:
             json.dump(self.map, f, indent=2)
         return f'{dir_path}/{self.seed}.json'
 
+    def generate_visible_chunks(self, player_chunk: tuple[int, int]):
+            count_chunk_col = int(self.camera.w / config.min_zoom // config.cell_size // config.chunk_size + 2)
+            count_chunk_row = int(self.camera.h / config.min_zoom // config.cell_size // config.chunk_size + 2)
+
+            left = player_chunk[0] - count_chunk_col // 2
+            top  = player_chunk[1] + count_chunk_row // 2
+
+            for x in range(count_chunk_col):
+                for y in range(count_chunk_col):
+                    self.map[f'{left + x}, {top - y}'] = self.map.get(f'{left + x}, {top - y}', self._create_chunk(left + x, top - y))
+
     def _create_chunk(self, left, top):
-        noise1 = PerlinNoise(octaves=3, seed=self.seed)
-        noise2 = PerlinNoise(octaves=6, seed=self.seed)
-        noise3 = PerlinNoise(octaves=12, seed=self.seed)
+        noise1 = PerlinNoise(octaves=3, seed=self.seed + left + top + left * top + random.randint(1 , 100))
+        noise2 = PerlinNoise(octaves=6, seed=self.seed + left + top + left * top + random.randint(1 , 100))
+        noise3 = PerlinNoise(octaves=12, seed=self.seed + left + top + left * top + random.randint(1 , 100))
 
         size = config.chunk_size
         chunk = {}
         for i in range(size):
             for j in range(size):
-                noise_val = noise1([i / size, j / size])
+
+                noise_val  = noise1([i / size, j / size])
                 noise_val += 0.5 * noise2([i / size, j / size])
                 noise_val += 0.25 * noise3([i / size, j / size])
 
@@ -63,7 +76,7 @@ class Map:
                     if v[0] < noise_val <= v[1]:
                         res_id = k
 
-                chunk[f'{(left + i) * config.cell_size}, {(top - j) * config.cell_size}'] = res_id
+                chunk[f'{(left * config.chunk_size + i) * config.cell_size}, {(top * config.chunk_size - j) * config.cell_size}'] = res_id
         return chunk
 
     def _get_player_coords(self):
@@ -73,14 +86,3 @@ class Map:
         left = self.player.x // config.cell_size // config.chunk_size
         top = self.player.y // config.cell_size // config.chunk_size
         return left, top
-
-    def generate_visible_chuncks(self):
-        count_chunk_col = self.camera.w / config.min_zoom // config.cell_size // config.chunk_size + 2
-        count_chunk_row = self.camera.h / config.min_zoom // config.cell_size // config.chunk_size + 2
-
-        left = self._get_player_chunk[0] + count_chunk_col // 2
-        top = self._get_player_chunk[1] + count_chunk_row // 2
-
-        for x in range(count_chunk_col):
-            for y in range(count_chunk_col):
-                self.map[f'{left + x}, {top - y}'] = self.map.get(f'{left + x}, {top - y}', self._create_chunk())
